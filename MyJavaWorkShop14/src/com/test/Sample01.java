33fa549a4714c1e7bc0e47fe24d2cc4741c62255
user <user@DESKTOP-T3DOGRQ>
2021-05-21 12:46:53 +0900

    ok

---
+++ MyJavaWorkShop14/src/com/test/Sample01.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.util.*;
+
+class Animal {
+	void breathing() {System.out.println("Animal");}
+}
+class Dog extends Animal {
+	void breathing() {System.out.println("Dog");}
+}
+public class Sample01 {
+	static void testMethod(List<? extends Animal>list) {
+		for(Animal a : list) {
+			a.breathing();
+		}
+	} 
+	public static void main(String[] args) {
+		testMethod(new ArrayList<Animal>());
+		List<Dog> dogs= new ArrayList<Dog>();
+		dogs.add(new Dog());
+		dogs.add(new Dog());
+		testMethod(dogs);
+		List<Animal> animals = new ArrayList<>();
+		animals.add(new Dog());
+		animals.add(new Dog());
+	}
+
+}
---
+++ MyJavaWorkShop14/src/com/test/Sample02.java
@@ -0,0 +1,26 @@
+package com.test;
+
+interface ICalculator{
+	public int add(int x, int y);
+}
+
+
+public class Sample02 {
+	public static void main(String[] args) {
+//		ICalculator calc =
+//				new ICalculator() {
+//					public int add(int a, int b) {
+//						return a+b;
+//			}
+//		};
+//		int c = calc.add(30, 50);
+//		System.out.println(c);
+	
+		ICalculator calc2 = (x,y)->{
+			return x+y;
+		};
+		int d = calc2.add(70, 100);
+		System.out.println(d);
+	}
+
+}
